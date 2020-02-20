b9e2bf6e0d289ba1764e7468dea4bd90c0cd9f65
deno101 <dennizwarui@gmail.com>
2020-02-20 14:24:11 +0300

    migration to MYSQL db

--- buxsbackend/settings.py
+++ buxsbackend/settings.py
@@ -73,13 +73,22 @@ WSGI_APPLICATION = 'buxsbackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'NAME': 'buxs_BE_test',
+        'USERNAME': 'deno101',
+        'PASSWORD': 'denniz'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
