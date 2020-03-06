16f9fef0c0192421c3303d7ec644542ee6f323ee
deno101 <dennizwarui@gmail.com>
2020-03-06 12:09:56 +0300

    fix

--- Test_Scripts/initdb.py
+++ Test_Scripts/initdb.py
@@ -32,7 +32,7 @@ def connect():
         price = random.randint(100, 1000)
         date_epoch = str(time.time())
         image_url = f'{i}.jpg'
-        query = f"INSERT INTO buxsbackend_marketplaceproducts VALUES('{id}','{name}','{price}','{date_epoch}','all','{image_url}',1)"
+        query = f"INSERT INTO market_place_marketplaceproducts VALUES('{id}','{name}','{price}','{date_epoch}','all','{image_url}',1)"
         cursor.execute(query)
         i += 1
         conn.commit()
