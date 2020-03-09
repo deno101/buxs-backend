747ce164a626ca9b4031aaa308a05ff82f7bf2f6
deno101 <dennizwarui@gmail.com>
2020-03-09 17:00:14 +0300

    fix bug

--- market_place/urls.py
+++ market_place/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('gdata/', views.get_mp),
     path('img/', views.get_img),
-    path('login/', views.login, name='login'),
+    path('login/', views.log_in, name='login'),
     path('updata/', views.upload_data, name='uplaod'),
 ]
