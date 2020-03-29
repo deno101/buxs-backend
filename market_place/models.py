2042d1c979505f8142a5f7201b5d0e4970564488
deno101 <dennizwarui@gmail.com>
2020-03-29 13:50:00 +0300

    fix /udata error cause by short length of description in db and form

--- market_place/models.py
+++ market_place/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 
 class MarketPlaceProducts(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, default='')
+    name = models.CharField(max_length=200, default='')
     price = models.CharField(max_length=50, default='')
     owner = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
     date_epoch = models.CharField(max_length=100, default='')
@@ -18,6 +18,6 @@ class MarketPlaceProducts(models.Model):
     image_url2 = models.CharField(max_length=100, default='')
     image_url3 = models.CharField(max_length=100, default='')
 
-    description = models.CharField(max_length=500, default='')
+    description = models.CharField(max_length=10000, default='')
     stock = models.IntegerField(default=50)
     brand = models.CharField(max_length=100, default='')
