303b70c026100eb5ed62d978c63d3bf1ec89a065
deno101 <dennizwarui@gmail.com>
2020-04-04 18:33:59 +0300

    fix server error 500

--- buxsbackend/settings.py
+++ buxsbackend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f65)cdir8f7p4=d%(11ip)kmt@zg+#hwy&_bz!$izr-bltz*y9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*'
